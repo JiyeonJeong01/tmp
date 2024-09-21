b119e7b5ba5d8ea8672d186c80001d74d92d3cdb
JiyeonJeong01 <jizzvibe@gmail.com>
2024-09-21 21:52:27 +0900

    file upload
    초기 파일 업로드

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX =  Input.GetAxis("Horizontal") * Time.deltaTime;
+
+        
+    }
+}
